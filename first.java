b7e7ec44751328f888e6c81251d8a090f1e565bb
arun.vy <arun.vy@hcl.com>
2022-12-30 15:25:34 +0000

    Added new block of code to file

--- first.java
+++ first.java
@@ -1,5 +1,6 @@
 THIS IS FIRST JAVA PROGRAiM
 
+PURPOSE : To print hello java using java program
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
